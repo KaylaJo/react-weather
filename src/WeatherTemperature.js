4b21204d2c81febecbace5a69cfd6f3545834284
KaylaJo <91999948+KaylaJo@users.noreply.github.com>
2022-01-03 23:15:44 -0600

    Added Temperature Conversion

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
 
 export default function WeatherTemperature(props) {
-  return (
-    <div>
-      <h2 className="todayTemp">{Math.round(props.celsius)}</h2>
-      <span className="degreeUnits">°C</span>
-    </div>
-  );
+  const [unit, setUnit] = useState(`metric`);
+
+  function convertToFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function convertToCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div>
+        <h2 className="todayTemp">{Math.round(props.celsius)}</h2>
+        <span className="unit">
+          °C |{" "}
+          <a href="/" onClick={convertToFahrenheit}>
+            °F
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    let fahrenheit = (props.celsius * 9) / 5 + 32;
+    return (
+      <div>
+        <h2 className="todayTemp">{Math.round(fahrenheit)}</h2>
+        <span className="unit">
+          <a href="/" onClick={convertToCelsius}>
+            °C
+          </a>
+          | °F
+        </span>
+      </div>
+    );
+  }
 }
